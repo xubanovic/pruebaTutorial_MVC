f74e6908f4741437828f6e92b11e1bf0928c954e
Xubanovic <xubanovic@gmail.com>
2024-04-12 11:53:21 +0200

    Home bukatuta

--- pruebaTutorial/Areas/Customer/Controllers/HomeController.cs
+++ pruebaTutorial/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using pruebaTutorialBook.DataAccess.Repository;
+using pruebaTutorialBook.DataAccess.Repository.IRepository;
 using pruebaTutorialBook.Models;
 using System.Diagnostics;
 
@@ -8,15 +10,23 @@ namespace pruebaTutorialBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
